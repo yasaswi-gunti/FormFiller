fcf24ab9534b1fb38a4575c0e5aa45eb65b9546e
Yasaswi Gunti <guntiyasaswi@gmail.com>
2025-07-16 10:41:37 +0530

    Replaced all special characters with - in outputfilename

--- src/utils.py
+++ src/utils.py
@@ -26,7 +26,7 @@ def setup_logger():
     os.makedirs(os.path.dirname(log_file),exist_ok=True)
 
     logger = logging.getLogger()
-    logger.setLevel(logging.DEBUG)
+    logger.setLevel(logging.INFO)
     handler = logging.FileHandler(log_file)
     formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
     handler.setFormatter(formatter)
